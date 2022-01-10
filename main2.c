5f97b7fe96dd07ed4cba1675c2bd09a55096cb97
salvatore <grimaldisalvatoreinginf@gmail.com>
2022-01-10 14:55:00 +0100

    refactor e inizializzazione array c locale

--- main2.c
+++ main2.c
@@ -18,6 +18,8 @@ int main(int argc, char ** argv){
     int max;
     int *full_vector;
     int *piece_of_vector;
+    int *c_local;
+    int lenC;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -26,74 +28,53 @@ int main(int argc, char ** argv){
     //printf("n_ranks=%d\n", n_ranks);
 
     if(rank == 0){
-        printf("Ciao sono proc. %d\n", rank);
+        //printf("Ciao sono proc. %d\n", rank);
         full_vector = (int *)malloc(N*sizeof(int)); 
         dim = N/n_ranks;
-        piece_of_vector = (int *)malloc(dim*sizeof(int));
-        for (i = 0; i < dim; i++){
-            piece_of_vector[i] = rand() % 5;
-            if(piece_of_vector[i] < local_min)
-                local_min = piece_of_vector[i]; 
-            else if(piece_of_vector[i] > local_max)
-                local_max = piece_of_vector[i];
-        }
-        for(i=0; i<dim; i++){
-            printf("%d ", piece_of_vector[i]);
-        }
-        printf("\n");
-        printf("min = %d\n", local_min);
-        printf("max = %d\n", local_max);
-        printf("\n\n");
-
     }
 
     if(rank > 0 && rank < n_ranks-1){
-        printf("Ciao sono proc. %d\n", rank);
+        //printf("Ciao sono proc. %d\n", rank);
         dim = N/n_ranks;
-        piece_of_vector = (int *)malloc(dim*sizeof(int));
-        for (i = 0; i < dim; i++){
-            piece_of_vector[i] = rand() % 10;
-            if(piece_of_vector[i] < local_min)
-                local_min = piece_of_vector[i]; 
-            else if(piece_of_vector[i] > local_max)
-                local_max = piece_of_vector[i];
-        }
-        for(i=0; i<dim; i++){
-            printf("%d ", piece_of_vector[i]);
-        }
-        printf("\n");
-        printf("min = %d\n", local_min);
-        printf("max = %d\n", local_max);
-        printf("\n\n");
     }
 
     if(rank == n_ranks-1){
-        printf("Ciao sono proc. %d\n", rank);
+        //printf("Ciao sono proc. %d\n", rank);
         dim = N/n_ranks + N % n_ranks;
-        piece_of_vector = (int *)malloc(dim*sizeof(int));
-        for (i = 0; i < dim; i++){
+    }
+
+    piece_of_vector = (int *)malloc(dim*sizeof(int));
+    for (i = 0; i < dim; i++){
             piece_of_vector[i] = rand() % 16;
             if(piece_of_vector[i] < local_min)
                 local_min = piece_of_vector[i]; 
             else if(piece_of_vector[i] > local_max)
                 local_max = piece_of_vector[i];
         }
-        for(i=0; i<dim; i++){
+        /*for(i=0; i<dim; i++){
             printf("%d ", piece_of_vector[i]);
         }
         printf("\n");
         printf("min = %d\n", local_min);
         printf("max = %d\n", local_max);
-        printf("\n\n");
-    }
+        printf("\n\n");*/
 
-    MPI_Allreduce(&local_min, &min, 1, MPI_INT, MPI_MIN, MPI_COMM_WORLD);
-    MPI_Allreduce(&local_max, &max, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
+        MPI_Allreduce(&local_min, &min, 1, MPI_INT, MPI_MIN, MPI_COMM_WORLD);
+        MPI_Allreduce(&local_max, &max, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
 
-    if(rank == 0){
+        if(rank == 0){
         printf("min = %d\n", min);
         printf("max = %d\n", max);
-    }
+        printf("\n\n");
+        }
+
+        lenC = max - min + 1;
+        c_local = (int *)malloc(lenC*sizeof(int));
+        for (i = 0; i < lenC_local; i++)
+            c_local[i] = 0;
+        
+        /*provare algoritmo mainS su carta e completare la parallelizzazione*/
+
 
     MPI_Finalize();
 
